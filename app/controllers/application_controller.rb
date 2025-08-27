2152ea31097f00f081c8c5c92fc9203bc1e138b9
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-08-27 00:25:13 -0400

    cop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,20 +6,20 @@ class ApplicationController < ActionController::Base
 
   def set_title(str = nil)
     @title = str
-    @title ||= params[:controller].to_s.capitalize + ' ' + params[:action].to_s.capitalize
+    @title ||= params[:controller].to_s.capitalize + " " + params[:action].to_s.capitalize
   end
 
   def set_metas
     @metas = [
-      {name: 'robots', content: 'index,follow'},
-      {name: 'keywords', content: "Master Patient Index,MPI,EMPI,FHIR,MCP,LLM integration,Health Technology"},
-      {name: 'description', content: "A master patient index with FHIR API and MCP server."},
-      {name: 'language', content: 'EN'},
+      { name: "robots", content: "index,follow" },
+      { name: "keywords", content: "Master Patient Index,MPI,EMPI,FHIR,MCP,LLM integration,Health Technology" },
+      { name: "description", content: "A master patient index with FHIR API and MCP server." },
+      { name: "language", content: "EN" }
     ]
   end
 
   def robots(index_or_follow)
-    idx = @metas.index {|x| x&.key?(:name) && (x[:name] == 'robots')}
+    idx = @metas.index { |x| x&.key?(:name) && (x[:name] == "robots") }
     @metas[0][:content] = index_or_follow
   end
 
