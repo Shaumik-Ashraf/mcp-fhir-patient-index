a56d955dc299341fb0e372f7f6959b256e1af0a0
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-08-29 11:50:14 -0400

    fix rspec patient failing tests

--- spec/views/patients/index.html.erb_spec.rb
+++ spec/views/patients/index.html.erb_spec.rb
@@ -5,14 +5,11 @@ RSpec.describe "patients/index", type: :view do
     assign(:patients, create_list(:patient, 2))
   end
 
-  it "renders a list of patients" do
+  it "renders a list of patients with uuids" do
     render
-    cell_selector = 'div>p'
-    visible_attributes = %w[first_name last_name email phone_number] # TODO: set visible attrs
+
     Patient.all.each do |patient|
-      visible_attributes.each do |attribute|
-        assert_select cell_selector, text: Regexp.new(patient.send(attribute))
-      end
+      expect(rendered).to match patient.uuid
     end
   end
 end
--- spec/views/patients/show.html.erb_spec.rb
+++ spec/views/patients/show.html.erb_spec.rb
@@ -2,45 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "patients/show", type: :view do
   let(:patient) { create(:patient) }
-  
+
   before(:each) do
-    assign(:patient, patient
-=begin
-    Patient.create!(
+    assign(:patient, Patient.create!(
       first_name: "First Name",
       last_name: "Last Name",
       administrative_gender: 2,
-      email: "Email",
-      phone_number: "Phone Number",
-      social_security_number: "Social Security Number",
-      address_line1: "Address Line1",
-      address_line2: "Address Line2",
-      address_city: "Address City",
-      address_state: "Address State",
-      address_zip_code: "Address Zip Code",
-      social_security_number: "Social Security Number",
-      passport_number: "Passport Number",
-      drivers_license_number: "Drivers License Number"
-=end
+      email: "Email@test.example.com",
+      phone_number: "1234567890",
+      address_line1: "123 Main St",
+      address_line2: "Apt 4",
+      address_city: "New York",
+      address_state: "NY",
+      address_zip_code: "55555",
+      social_security_number: "000-00-0000",
+      passport_number: "P88888888",
+      drivers_license_number: "D7777777"
     ))
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes" do
     render
-    # TODO: hard code realistic attributes above and assert them below
+
     expect(rendered).to match(/First Name/)
     expect(rendered).to match(/Last Name/)
     expect(rendered).to match(/2/)
-    expect(rendered).to match(/Email/)
-    expect(rendered).to match(/Phone Number/)
-    expect(rendered).to match(/Social Security Number/)
-    expect(rendered).to match(/Address Line1/)
-    expect(rendered).to match(/Address Line2/)
-    expect(rendered).to match(/Address City/)
-    expect(rendered).to match(/Address State/)
-    expect(rendered).to match(/Address Zip Code/)
-    expect(rendered).to match(/Social Security Number/)
-    expect(rendered).to match(/Passport Number/)
-    expect(rendered).to match(/Drivers License Number/)
+    expect(rendered).to match(/Email@test\.example\.com/)
+    expect(rendered).to match(/1234567890/)
+    expect(rendered).to match(/123 Main St/)
+    expect(rendered).to match(/Apt 4/)
+    expect(rendered).to match(/New York/)
+    expect(rendered).to match(/NY/)
+    expect(rendered).to match(/55555/)
+    expect(rendered).to match(/000-00-0000/)
+    expect(rendered).to match(/P88888888/)
+    expect(rendered).to match(/D7777777/)
   end
 end
