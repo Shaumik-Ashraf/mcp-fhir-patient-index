66fb0a6747362c1f4a799642872e330db2d6e3b4
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-09-10 18:11:56 -0400

    wip: fhir patient read spec

---
+++ spec/requests/fhir/r4/patient_read_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "/fhir/r4/Patient/:uuid", type: :request do
+  before(:each) do
+    create(:patient, first_name: "PATIENT_READ_TEST")
+  end
+
+  let(:patient) { Patient.find_by!(first_name: "PATIENT_READ_TEST") }
+
+  it "returns 200 ok" do
+    get fhir_r4_patient_url(patient)
+    expect(response).to be_successful
+  end
+
+  it "returns a FHIR Patient" do
+    get fhir_r4_patient_url(patient)
+    expect(FHIR.from_contents(response.body)).to be_instance_of FHIR::Patient
+  end
+
+  it "returns identical FHIR logical id" do
+    get fhir_r4_patient_url(patient)
+    expect(FHIR.from_contents(response.body).id).to eq patient.uuid
+  end
+end
