b433a23960b3435ef8dbcf5a367c0861fd1d64c7
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-10-06 17:25:21 -0400

    refactoring mcp impl

--- app/controllers/mcp/v20250618/application_controller.rb
+++ app/controllers/mcp/v20250618/application_controller.rb
@@ -2,9 +2,10 @@ module MCP # NOTE: namespace collision with MCP gem
   module V20250618
     class ApplicationController < ::ActionController::Base
       skip_forgery_protection
+      include ActionMCP
 
       def index
-        render json: ActionMCP.handle_json(request)
+        render json: mcp_streamable_http.handle_json(request.body.read)
       end
     end
   end
--- app/mcp/action_mcp.rb
+++ app/mcp/action_mcp.rb
@@ -2,70 +2,147 @@ Dir.glob("**/*", base: __dir__).each { |dependency| require_relative dependency 
 
 # @example
 #   class MyController < ApplicationController
+#     include ActionMCP
 #     def index
-#       render json: ActionMCP.handle_json(request)
+#       render json: mcp_streamable_http.handle_json(request.body.read)
 #     end
 #   end
-class ActionMCP
+#
+# @example
+#   class CLI
+#     include ActionMCP
+#     def run
+#       mcp_stdio.open
+#     end
+#   end
+module ActionMCP
   SCHEME = "master-patient-index"
+  NAME = Rails.application.name.underscore
+  TITLE = Rails.application.name.humanize
+  VERSION = "0.0.0" # TODO: consolidate versions across app, fhir, and mcp
+  INSTRUCTIONS = <<~EOT
+    See https://github.com/Shaumik-Ashraf/mcp-fhir-patient-index/README.md
+  EOT
+  ID_REGEX = "[A-Za-z0-9_-]+"
 
-  @@server = MCP::Server.new(
-    name: "master_patient_index_mcp_server",
-    title: "Master Patient Index",
-    version: "0.0.0", # TODO: consolidate versions across app, fhir, and mcp
-    instructions: "See https://github.com/Shaumik-Ashraf/mcp-fhir-patient-index/README.md",
-    tools: [],
-    prompts: [],
-    resources: [
-      ApplicationResource.new(
-        uri: "#{SCHEME}://info",
-        name: "info_resource",
-        title: "Patient Resource",
-        description: "MCP server resource for Patient Records.",
-        mime_type: "text/plain"
-      ),
-      ApplicationResource.new(
-        uri: "#{SCHEME}://all",
-        name: "all_patients_resource",
-        title: "All Patient Resources",
-        description: "All patient resources returned at once. This resource will not require pagination.",
-        mime_type: "text/plain"
-      )
-    ],
-    resource_templates: [
-      ApplicationResourceTemplate.new(
-        uri_template: "#{SCHEME}://patient/{uuid}",
-        name: "patient_resource_template",
-        title: "Patient Resource parameterized by UUID",
-        description: "Patient resource by primary id (UUID)",
-        mime_type: "text/plain"
-      )
-    ],
-    server_context: {} # TODO https://github.com/modelcontextprotocol/ruby-sdk?tab=readme-ov-file#server_context
-  )
+  @tools = []
+  @prompts = []
+  @resources = []
+  @resource_templates = []
 
-  @@server.resources_read_handler do |params|
-    case params[:uri]
-    when "#{SCHEME}://info"
-      [{ uri: "#{SCHEME}://info", mimeType: "text/plain", text: "This is a master patient index server that supports MCP." }]
-    when "#{SCHEME}://all"
-      [{ uri: "#{SCHEME}://all", mimeType: "text/plain", text: PatientRecord.all.map(&:to_text).join("\n\n") }]
-    when %r[#{Regexp.escape(SCHEME)}://patient]
-      [{ uri: "#{SCHEME}://patient", mimeType: "text/plain", text: PatientRecord.find_by!(uuid: params[:uuid]).to_text }]
-    else
-      # TODO
+  def define_resource(endpoint, mime_type, title=nil, description=nil, &block)
+    @resources ||= []
+    @resources << {
+      MCP::Resource.new(
+        uri: "#{SCHEME}://#{endpoint}",
+        name: endpoint.to_s.underscore,
+        title:,
+        description:,
+        mime_type:
+      ) => block
+    }
+  end
+
+  def load_application_records_as_resource_templates(*klasses)
+    klasses.each do |klass|
+      @resource_templates << Struct.new(
+        mcp_resource_template: MCP::ResourceTemplate.new(
+          uri: "#{SCHEME}://#{klass.underscore.singularize}/{id}",
+          name: klass.to_s.underscore.singularize,
+          title: klass.to_s.humanize,
+          # omit description
+          mime_type: case klass
+                     when ->(k) { k.instance_methods.include? :to_text }
+                       "text/plain"
+                     when ->(k) { k.instance_methods.include? :to_json }
+                       "text/json"
+                     else
+                       raise StandardError, "ApplicationRecord #{klass} does not respond to #to_text or #to_json"
+                     end
+        ),
+        uri_regex: %r{#{Regexp.escape(klass.underscore.singularize)}/(#{ID_REGEX})},
+        blk: ->(params) { klass.find(params[:id]) }
+      )
     end
   end
 
-  # @param request [ActionDispatch::Request]
-  # @return [#as_json] MCP response
-  def self.handle_json(request)
-    @@server.handle_json(request.body.read)
-  rescue ActiveRecord::RecordNotFound => e
-    Rails.logger.warn "ActionMCP Error: #{e}"
-    { jsonrpc: "2.0", id: request.params[:id], error: { code: -32002, message: "Resource not found" } }
-  rescue StandardError => e
-    Rails.logger.error "ActionMCP Error: #{e}"
-    { jsonrpc: "2.0", id: request.params[:id], error: { code: -32603, message: e.full_message, data: e.to_hash } }
+  def resource_template_uri(klass)
+    "#{reource_template_uri_prefix}/{id}"
+  end
+
+  def resource_template_uri_prefix(klass)
+    "#{SCHEME}://#{klass.underscore.singularize}"
+  end
+
+  def server
+    define_resource "info", "text/plain", "MCP Server Information" do
+      <<~EOT
+        This is a master patient index server that supports model context
+        protocol (MCP). It contains patient records of demographic information
+        for retrieval, matching, and de-duplication.
+      EOT
+    end
+
+    define_resource "all", "text/plain", "All", "Retrieve all patient records" do
+      PatientRecord.all.map(&:to_text).join("\n")
+    end
+
+    load_application_records_as_resource_templates(PatientRecord)
+
+    # TODO: create patient tool
+
+    srv = MCP::Server.new(
+      name: NAME,
+      title: TITLE,
+      version: VERSION,
+      instructions: INSTRUCTIONS,
+      tools: [],
+      prompts: [],
+      resources: @resources.keys,
+      resource_templates: [
+        ApplicationResourceTemplate.new(
+          uri_template: "#{SCHEME}://patient/{uuid}",
+          name: "patient_resource_template",
+          title: "Patient Resource parameterized by UUID",
+          description: "Patient resource by primary id (UUID)",
+          mime_type: "text/plain"
+        )
+      ],
+      server_context: {} # TODO https://github.com/modelcontextprotocol/ruby-sdk?tab=readme-ov-file#server_context
+    )
+
+    srv.resources_read_handler do |params|
+      @resources.each do |mcp_resource, blk|
+        if mcp_resource.uri == params[:uri]
+          return blk.call
+        end
+      end
+
+      @resource_templates.each do |struct|
+        if struct.uri_regex.match? params[:uri]
+          return struct.blk.call
+        end
+      end
+
+      raise StandardError, "MCP Parameters did not map to a tool, prompt, resource, or resource template: #{params}"
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.warn "ActionMCP Error: #{e}"
+      { jsonrpc: "2.0",
+        id: request.params[:id],
+        error: { code: -32002, message: "Resource not found" } }
+    rescue StandardError => e
+      Rails.logger.error "ActionMCP Error: #{e}"
+      { jsonrpc: "2.0",
+        id: request.params[:id],
+        error: { code: -32603, message: e.full_message, data: e.to_hash } }
+    end
+  end
+
+  def mcp_streamable_http
+    server
+  end
+
+  def mcp_stdio
+    MCP::Server::Transports::StdioTransport.new(server)
   end
 end
