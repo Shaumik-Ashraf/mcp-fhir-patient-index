64c0ff4537ceb0a26cfc5de166a22584641e9040
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-10-17 20:12:35 -0400

    cop

--- db/migrate/20251017220948_create_snapshots_tables.rb
+++ db/migrate/20251017220948_create_snapshots_tables.rb
@@ -1,12 +1,11 @@
 class CreateSnapshotsTables < ActiveRecord::Migration::Current
-
   def change
     create_table :snapshots do |t|
       t.belongs_to :item, polymorphic: true, null: false, index: true
       t.belongs_to :user, polymorphic: true
 
       t.string :identifier, index: true
-      t.index [:identifier, :item_id, :item_type], unique: true
+      t.index [ :identifier, :item_id, :item_type ], unique: true
 
       t.json :metadata
 
@@ -16,7 +15,7 @@ class CreateSnapshotsTables < ActiveRecord::Migration::Current
     create_table :snapshot_items do |t|
       t.belongs_to :snapshot, null: false, index: true
       t.belongs_to :item, polymorphic: true, null: false, index: true
-      t.index [:snapshot_id, :item_id, :item_type], unique: true
+      t.index [ :snapshot_id, :item_id, :item_type ], unique: true
 
       t.json :object, null: false
 
@@ -24,5 +23,4 @@ class CreateSnapshotsTables < ActiveRecord::Migration::Current
       t.string :child_group_name
     end
   end
-
 end
