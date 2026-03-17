b67afe9c597edf1892dac96390a2092058552f3f
Shaumik-Ashraf <a.shaumik@gmail.com>
2026-03-17 16:57:25 -0700

    fix rspec for patient index

--- spec/views/patient_records/index.html.erb_spec.rb
+++ spec/views/patient_records/index.html.erb_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 RSpec.describe "patient_records/index", type: :view do
   before do
     assign(:patient_records, create_list(:patient, 2))
+    render
   end
 
-  it "renders a list of patients with uuids" do
-    render
+  it "renders the patient grid container" do
+    expect(rendered).to have_selector "#patient-grid"
+  end
 
+  it "renders a link to create a new patient" do
+    expect(rendered).to have_link "New Patient"
+  end
+
+  it "does not inline patient data" do
     PatientRecord.all.each do |patient|
-      expect(rendered).to match patient.uuid
+      expect(rendered).not_to match patient.uuid
     end
   end
 end
